57963e1250e853d71627e64d37f9bdae23282edd
Cleviane <crsilva@sfiec.org.br>
2022-05-09 18:06:47 -0300

    fixing imagePath prop

--- src/pages/orderEntry/components/Options/Scoops/index.jsx
+++ src/pages/orderEntry/components/Options/Scoops/index.jsx
@@ -1,9 +1,9 @@
 import Col from "react-bootstrap/Col";
 
-const Scoops = ({ item: { name, imageUrl } }) => {
+const Scoops = ({ item: { name, imagePath } }) => {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
-      <img src={`http://localhost:3030/${imageUrl}`} alt={`${name} scoop`} />
+      <img src={`http://localhost:3030/${imagePath}`} alt={`${name} scoop`} />
     </Col>
   );
 };
--- src/pages/orderEntry/components/Options/Toppings/index.jsx
+++ src/pages/orderEntry/components/Options/Toppings/index.jsx
@@ -1,9 +1,9 @@
 import Col from "react-bootstrap/Col";
 
-const Toppings = ({ item: { name, imageUrl } }) => {
+const Toppings = ({ item: { name, imagePath } }) => {
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: "center" }}>
-      <img src={`http://localhost:3030/${imageUrl}`} alt={`${name} topping`} />
+      <img src={`http://localhost:3030/${imagePath}`} alt={`${name} topping`} />
     </Col>
   );
 };
